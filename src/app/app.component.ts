12dc48371371c15319c85cbe8484b83084119bba
RavanIsmayilov <rvanismayilov70@gmail.com>
2024-04-17 00:13:54 +0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'home-work5';
+    todo: string = '';
+    todos: { text: string, deleted: boolean }[] = [];
+
+    add() {
+        this.todos.push({ text: this.todo, deleted: false })
+        this.todo = '';
+    }
+
+    remove(index: number) {
+        if (confirm('Are you sure you want to delete this item?')) {
+            this.todos[index].deleted = true;
+            setTimeout(() => {
+                this.todos.splice(index, 1);
+            }, 50000); 
+        }
+    }
 }
